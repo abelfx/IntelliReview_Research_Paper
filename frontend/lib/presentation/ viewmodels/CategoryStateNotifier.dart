c4df498e458620f9d9fe27ea889e94089e6003c9
nahom zenebe <nahomzenebe123@gmail.com>
2025-05-15 20:07:34 +0300

    adding statenotifier for category

---
+++ frontend/lib/presentation/ viewmodels/CategoryStateNotifier.dart
@@ -0,0 +1,48 @@
+
+
+
+
+import 'package:frontend/application/providers/category_provider.dart';
+import 'package:frontend/domain/entities/Categoryentities.dart';
+import 'package:frontend/domain/usecases/Categoryusecase.dart';
+import 'package:riverpod/riverpod.dart';
+
+class CategoryNotifier extends StateNotifier<AsyncValue<List<Categoryentities>>> {
+  final Categoryusecase categoryUseCase;
+
+  CategoryNotifier(this.categoryUseCase) : super(const AsyncLoading()) {
+    getAllCategories();
+  }
+
+  Future<void> getAllCategories() async {
+    try {
+      final categories = await categoryUseCase.getallCategory();
+      state = AsyncData(categories);
+    } catch (e, st) {
+      state = AsyncError(e, st);
+    }
+  }
+
+  Future<void> addCategory(String name, String description) async {
+    try {
+      await categoryUseCase.createCategory(name, description);
+      await getAllCategories();
+    } catch (e, st) {
+      state = AsyncError(e, st);
+    }
+  }
+
+  Future<void> removeCategory(String id) async {
+    try {
+      await categoryUseCase.deleteCategory(id);
+      await getAllCategories();
+    } catch (e, st) {
+      state = AsyncError(e, st);
+    }
+  }
+}
+
+final categoryNotifierProvider = StateNotifierProvider<CategoryNotifier, AsyncValue<List<Categoryentities>>>((ref) {
+  final useCase = ref.watch(categoryUseCaseProvider);
+  return CategoryNotifier(useCase);
+});
