af854a75922190443d9b1933541b6ee99d94bc2c
aiazar <alazarand@gmail.com>
2025-06-08 19:43:31 +0300

    import issue fixed in notification card widget

--- lib/presentation/widgets/notification_card.dart
+++ lib/presentation/widgets/notification_card.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../models/notification_model.dart';
+import '../../data/models/notification_model.dart';
 
 class NotificationCard extends StatelessWidget {
   final NotificationModel notification;
